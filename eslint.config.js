bbc9f4da72a9e9be387f21f83af6ef92f78db43b
Samuel Frías <samuelfriher@gmail.com>
2025-11-29 13:13:13 +0000

    Replace ESLint configuration with a new setup in eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const prettierPlugin = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/', 'output/'],
+  }
+);
